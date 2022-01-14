fd25bdb3e6fb1bbc1ed5a2c191e9806ae0fad4eb
ifTNT <iftnt1999@gmail.com>
2022-01-14 22:31:35 +0800

    Enhance error displaying on mongodb failure

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const ExpressServer = require("./expressServer");
 const mongoose = require("mongoose");
 
 const launchServer = async () => {
+  mongoose
+    .connect(db_config.URI, {
+      serverSelectionTimeoutMS: 5000,
+    })
+    .catch((error) =>
+      logger.error("[MongoDB] Failed connecting database ", error)
+    );
   try {
-    await mongoose.connect(db_config.URI);
     this.expressServer = new ExpressServer(http_config);
     this.expressServer.launch();
     logger.info("[Express] Server is running");
   } catch (error) {
-    logger.error("[Express] Server failure", error.message, error.stack);
+    logger.error("[Express] Server failure ", error.message, error.stack);
     await this.expressServer.close();
   }
 };
